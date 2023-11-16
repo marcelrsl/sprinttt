20ad3a6c7f78fe8f000ab7888ba992b9826c6295
marcelrsl <111505325+marcelrsl@users.noreply.github.com>
2023-11-16 17:41:38 +0100

    Kommentare caesar

--- Monkeyss/src/main/java/Monkeys/VerschluesselungController.java
+++ Monkeyss/src/main/java/Monkeys/VerschluesselungController.java
@@ -15,7 +15,7 @@ public class VerschluesselungController  {
         String ciphertext = "";
         plaintext = plaintext.toLowerCase(); //Alles in Kleinbuchstaben umwandeln
         for(int i=0; i<plaintext.length(); i++){
-            //Buchstaben verschieben und an Geheimtext haengen
+            //Buchstaben verschieben und an Geheimtext hinzufügen
             char next = plaintext.charAt(i);
             ciphertext = ciphertext + shift(next, key);
         }
@@ -25,32 +25,31 @@ public class VerschluesselungController  {
     //Verschiebt den Buchstaben um die angegebene Verschiebung (key)
     private char shift(char letter, int shift){
         //Buchstaben als Zahl behandeln
-        if(letter>='a' && letter <='z'){ //Sonderzeichen nicht veraendern
+        if(letter>='a' && letter <='z'){ //Sonderzeichen nicht verändern
             letter += shift;
             while(letter > 'z'){
-                letter -= 26;
+                letter -= 26; //Es wird sicher gestellt, dass nach dem "Z", man wieder zum Beginn des Alphabets zurückkehrt
             }
         }
         return letter;
     }
 
-    //Entschluesselt den Geheimtext mit dem angegebenen Schluessel 
+    //Entschlüsselt den Geheimtext mit dem angegebenen Schlüssel 
     private String decode(String ciphertext, int key){
         String decoded = "";
-        ciphertext = ciphertext.toLowerCase(); //Alles in Kleinbuchstaben umwandeln
+        ciphertext = ciphertext.toLowerCase(); //Geheimtext in Kleinbuchstaben umwandeln
         for(int i=0; i<ciphertext.length(); i++){
-            //Jeden Buchstaben verschieben und zwar um (26-Schluessel(key)) Stellen
-            //und an entschluesselten Text anhaengen
+            //Jeden Buchstaben verschieben, um 26 Schlüssel Positonen und zum entschlüsselten Text hinzufügen
            char next = ciphertext.charAt(i);
            decoded = decoded + shift(next, 26-key);
         }
         return decoded;
     }
 
-    @GetMapping("/caesar")
+    @GetMapping("/caesar") 
     public String caesar(@RequestParam(name="encrypt", required = false, defaultValue = "") String encrypt,@RequestParam(name="klartext", required = false, defaultValue = "") String klartext,Model model) {
-        model.addAttribute("activePage", "caesar");
-        model.addAttribute("klartext", klartext);
+        model.addAttribute("activePage", "caesar"); //Hinzufügen der Attribute für die active Page
+        model.addAttribute("klartext", klartext); //Der Klartet wird als Attribut hinzugefügt
         model.addAttribute("encrypt", klartext.length() > 0);
     
         return "index.html";
@@ -61,7 +60,7 @@ public String caesarDo(RedirectAttributes redirectAttributes, @RequestParam(name
     @RequestParam(name="key", required = true, defaultValue = "0") int key,
     Model model) {
     String encryptedText = code(klartext, key);
-    redirectAttributes.addAttribute("klartext", encryptedText);
+    redirectAttributes.addAttribute("klartext", encryptedText); //Verschlüsselter Text wird als Attribut weitergeleitet
     redirectAttributes.addAttribute("encrypt", klartext.length() > 0);
     return "redirect:/caesar"; // Annahme, dass deine HTML-Datei "caesar.html" ist, ändere dies entsprechend
     }
